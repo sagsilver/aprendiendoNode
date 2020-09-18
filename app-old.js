bfea57ce647278165656a190a8544d325ffe8e12
sagsilver <s.sagsilver@gmail.com>
2020-09-18 12:17:48 -0500

    Pirmer commit

---
+++ app-old.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    let salida = {
+      nombre: "Santiago",
+      edad: "26",
+      url: req.url,
+    };
+    res.write(JSON.stringify(salida));
+    // res.write("hola mundo");
+    res.end();
+  })
+  .listen(8080);
+console.log("Escuchando puerto 8080");
